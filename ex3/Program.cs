651b691f87bb5da87e6164ddaaf2789561908353
Warit Powtongsook <60293343+GookGai@users.noreply.github.com>
2021-09-23 15:10:14 +0700

    Add files via upload

---
+++ ex3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace Lab_Os_Concurrency02{
+    class Program{
+        static int resource = 10000;
+        static void TestThread1(){
+           resource = 55555;
+        }
+        static void Main(string[] args){
+            Thread th1 = new Thread(TestThread1);
+            th1.Start();
+              Thread.Sleep(1000);
+            Console.WriteLine("resource={0}",resource);
+        }
+    }
+}
---
+++ ex4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace Lab_Os_Concurrency02{
+    class Program{
+        static int resource = 10000;
+        static void TestThread1(){
+          int i;
+            for (i=0;i<45555;i++){
+                resource++;
+                Console.Write(".");
+            }
+        }
+        static void Main(string[] args){
+            Thread th1 = new Thread(TestThread1);
+            th1.Start();
+              Thread.Sleep(10);
+            Console.WriteLine("resource={0}",resource);
+        }
+    }
+}
